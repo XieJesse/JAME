2d733d7161bae631e5fd180cddc6c43ac5cdbbbd
JesseStuy20 <jxie20@stuy.edu>
2024-02-17 14:42:37 -0500

    added database functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react'
-import { db, auth, storage } from './config/firebase'
-import { getDocs, getDoc, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore'
+// import { db, auth } from './config/firebase'
+// import { getDocs, getDoc, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div> 
+      <div>
           <p className="text-red-400 font-bold">hi</p>
           <h1 className="font-bold underline">
-      Hello world!
-    </h1>
+          Hello world!
+          </h1>
+
       </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { db, auth } from '../../config/firebase'
+import { getDocs, getDoc, collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore'
 
 const Map = () => {
+
+  const [count, setCount] = useState(0) ;
+
+  const [eventTitle, setEventTitle] = useState("") ;
+  const [eventHost, setEventHost] = useState("") ;
+  const [eventStart, setEventStart] = useState("") ;
+  const [eventEnd, setEventEnd] = useState("") ;
+  const [eventAddress, setEventAddress] = useState("") ;
+  const [eventCoordinates, setEventCoordinates] = useState("") ;
+  const [eventDescription, setEventDescription] = useState("") ;
+  const [eventTags,setEventTags] = useState([]) ;
+
+  const [pinsList, setPinsList] = useState([]) ;
+
+  const pinCollection = collection(db, "pins") ;
+
+  const getPins = async () => {
+    try {
+      const pins = await getDocs(pinCollection) ;
+      setPinsList(pins.docs.map((doc) => ({
+        ...doc.data(),
+      })))
+      console.log(pinsList)
+      // update pins list from database
+    } catch (error) {
+      console.log(error) ;
+    }
+
+  }
+
+  const addPin = async () => {
+    try {
+      await addDoc(pinCollection,{
+        title: eventTitle,
+        host: eventHost,
+        start: eventStart,
+        end: eventEnd,
+        address: eventAddress,
+        coordinates: eventCoordinates,
+        description: eventDescription,
+        tags: eventTags,
+        }
+      ) ;
+      getPins() ;
+    } catch (error) {
+      console.log(error) ;
+    }
+  }
+  useEffect(() => {
+    getPins()
+  }, []) ;
+
   return (
-    <div>map page</div>
+    <div>
+      <div>
+        {pinsList.map((pin) => (
+          <>
+            <h1> title: {pin.title} </h1>
+              host: {pin.host}
+              start: {pin.start}
+          </>
+
+        ))}
+      </div>
+
+    </div>
   )
 }
 
