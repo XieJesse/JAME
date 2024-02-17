9bd7b7ae15cfbdbb0135358eb98300d08502fd2f
JesseStuy20 <jxie20@stuy.edu>
2024-02-17 16:55:08 -0500

    Added database functionality

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -14,7 +14,26 @@ const Map = () => {
   const [eventAddress, setEventAddress] = useState("") ;
   const [eventCoordinates, setEventCoordinates] = useState("") ;
   const [eventDescription, setEventDescription] = useState("") ;
-  const [eventTags,setEventTags] = useState([]) ;
+  const [eventTags,setEventTags] = useState([{
+            field: 'Club Meeting',
+            checked: false
+        },
+        {
+            field: 'Sporting Event',
+            checked: false
+        },
+        {
+            field: 'Cultural Event',
+            checked: false
+        }
+    ]
+  )
+
+  const updateTags = (tag, isChecked) => {
+      const tagsList = [...eventTags];
+      tagsList[tag].checked = isChecked;
+      setEventTags(tagsList);
+  }
 
   const [pinsList, setPinsList] = useState([]) ;
 
@@ -24,9 +43,11 @@ const Map = () => {
     try {
       const pins = await getDocs(pinCollection) ;
       setPinsList(pins.docs.map((doc) => ({
+        id: doc.id,
         ...doc.data(),
       })))
-      console.log(pinsList)
+      console.log(pinsList) ;
+      console.log("dasdad") ;
       // update pins list from database
     } catch (error) {
       console.log(error) ;
@@ -34,7 +55,8 @@ const Map = () => {
 
   }
 
-  const addPin = async () => {
+  const addPin = async (e) => {
+    e.preventDefault()
     try {
       await addDoc(pinCollection,{
         title: eventTitle,
@@ -48,10 +70,31 @@ const Map = () => {
         }
       ) ;
       getPins() ;
+      setEventTitle("")
+      setEventHost("")
+      setEventStart("")
+      setEventEnd("")
+      setEventAddress("")
+      setEventCoordinates("")
+      setEventDescription("")
+      setEventTags([{
+                field: 'Club Meeting',
+                checked: false
+            },
+            {
+                field: 'Sporting Event',
+                checked: false
+            },
+            {
+                field: 'Cultural Event',
+                checked: false
+            }
+      ])
     } catch (error) {
       console.log(error) ;
     }
   }
+
   useEffect(() => {
     getPins()
   }, []) ;
@@ -60,15 +103,53 @@ const Map = () => {
     <div>
       <div>
         {pinsList.map((pin) => (
-          <>
+          <div key={pin.id}>
             <h1> title: {pin.title} </h1>
-              host: {pin.host}
-              start: {pin.start}
-          </>
+              <p> host: {pin.host} </p>
+              <p> start: {pin.start} </p>
+              <p> end: {pin.end} </p>
+              <p> address: {pin.address} </p>
+              <p> coordinates: {pin.coordinates} </p>
+              <p> description: {pin.description} </p>
+              <p> tags: </p>
 
+              {pin.tags.map((tag,index) => (
+                <>
+                  {tag.checked &&
+                    <p> - {tag.field} </p>
+                  }
+                </>
+              ))}
+
+              <br/>
+          </div>
         ))}
       </div>
+      <form className="flex flex-col" onSubmit={addPin}>
+        <input value={eventTitle} type="text" placeholder="title" onChange={(e) => setEventTitle(e.target.value)} />
+        <input value={eventHost} type="text" placeholder="host" onChange={(e) => setEventHost(e.target.value)} />
+        <input value={eventStart} type="datetime-local" placeholder="start" onChange={(e) => setEventStart(e.target.value)} />
+        <input value={eventEnd} type="datetime-local" placeholder="end" onChange={(e) => setEventEnd(e.target.value)} />
+        <input value={eventAddress} type="text" placeholder="address" onChange={(e) => setEventAddress(e.target.value)} />
+        <input value={eventCoordinates} type="text" placeholder="coordinates" onChange={(e) => setEventCoordinates(e.target.value)} />
+        <input value={eventDescription} type="text" placeholder="description" onChange={(e) => setEventDescription(e.target.value)} />
 
+        {
+        eventTags.map((tag, index) =>
+          <div key = {tag.field}>
+            <label>
+              <span>{tag.field}</span>
+              <input key={index}
+                 type="checkbox"
+                 checked={tag.checked}
+                 onChange={() => updateTags(index, !tag.checked)}
+              />
+            </label>
+          </div>
+        )}
+
+        <input type="submit" value="submit" />
+      </form>
     </div>
   )
 }
