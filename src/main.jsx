ecbc82d2d61245852ccd88dd42c289e5c5ea1432
antqiu <anthonyqiuchu@gmail.com>
2024-02-17 16:33:04 -0500

    get rid of login

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Outlet, BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
-import Auth from "./pages/auth/index" 
 import Map from "./pages/map/index" 
 import Layout from "./pages/Layout" 
 import Homepage from "./pages/homepage/index"
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index={true} element={<Homepage />} />
-          <Route path="/login" index={false} element={<Auth />} />
           <Route path="/map" index={false} element={<Map />} />
           <Route path="/about" index={false} element={<About />} />
         </Route>
